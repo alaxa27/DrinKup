3ddd4f94fe7bdcfb58acdaf525d815e2abdf9352
SAKMECHE Taqiy <alaxa2@gmail.com>
2016-09-27 09:38:31 +0200

    nouveau fichier : www/img/tekpaf.jpg 	modifié :         www/js/controllers.js 	modifié :         www/js/services.js 	nouveau fichier : www/templates/modal-products.html 	modifié :         www/templates/tab-panier.html 	modifié :         www/templates/tab-products.html Packs have been added.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,25 +1,64 @@
 angular.module('starter.controllers', [])
 
-.controller('ProductsCtrl', function($scope, $state, Chats, Panier) {
+.controller('ProductsCtrl', function($scope, $state, $ionicModal, Chats, Panier, Categories) {
+  $scope.categories = Categories.all();
   $scope.panier = Panier.all();
   $scope.products = Chats.all();
+
   $scope.get_number = function(ID) {
     return Panier.get_number(ID);
   };
+
   $scope.add_to_panier = function (ID) {
     Panier.add_to_panier(ID);
   };
+
   $scope.remove_from_panier = function (ID) {
     Panier.remove_from_panier(ID);
   };
 
-  $scope.go_to_panier = function() {
-    $state.go('tab.panier');
-  };
+  $ionicModal.fromTemplateUrl('templates/modal-products.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  $scope.show_modal = function (ID) {
+    $scope.modal.show();
+    $scope.modal.scope.product = $scope.products[ID];
+    $scope.modal.scope.products = $scope.products;
+    $scope.modal.scope.on_swipe_down = function () {
+      $scope.modal.hide();
+    };
+
+    $scope.modal.scope.add_to_panier = function (ID) {
+      Panier.add_to_panier(ID);
+      $scope.modal.scope.pack_size = Panier.get_panier(ID).size;
+    };
+
+    $scope.modal.scope.remove_from_panier = function (ID) {
+      Panier.remove_from_panier(ID);
+    };
+
+    $scope.modal.scope.push_to_panier = function (ID, obj) {
+      Panier.push_to_panier(ID, obj);
+
+    };
+
+    $scope.modal.scope.get_number = function(ID) {
+      return Panier.get_number(ID);
+    };
+
+  }
+
+  /*$scope.createContact = function(u) {
+    $scope.contacts.push({ name: u.firstName + ' ' + u.lastName });
+    $scope.modal.hide();
+  };*/
 
 })
 
-.controller('PanierCtrl', function($scope, Panier, Chats) {
+.controller('PanierCtrl', function($scope, $ionicModal, Panier, Chats) {
   $scope.panier = Panier.all();
   $scope.products = Chats.all();
   $scope.get_product = function (ID) {
@@ -36,6 +75,41 @@ angular.module('starter.controllers', [])
   };
   for (var i; i < panier.length; i++) {
 
+  }
+
+  $ionicModal.fromTemplateUrl('templates/modal-products.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  $scope.show_modal = function (ID) {
+    $scope.modal.show();
+    $scope.modal.scope.products = $scope.products;
+    $scope.modal.scope.product = $scope.products[ID];
+
+    $scope.modal.scope.on_swipe_down = function () {
+      $scope.modal.hide();
+    };
+
+    $scope.modal.scope.add_to_panier = function (ID) {
+      Panier.add_to_panier(ID);
+    };
+
+    $scope.modal.scope.pack_size = Panier.get_panier(ID).size;
+
+    $scope.modal.scope.remove_from_panier = function (ID) {
+      Panier.remove_from_panier(ID);
+    };
+
+    $scope.modal.scope.push_to_panier = function (ID, obj) {
+      Panier.push_to_panier(ID, obj);
+
+    };
+
+    $scope.modal.scope.get_number = function(ID) {
+      return Panier.get_number(ID);
+    };
   }
 })
 
--- www/js/services.js
+++ www/js/services.js
@@ -20,9 +20,23 @@ angular.module('starter.services', [])
           return 1;
         }
       }
-      var prod = '{"id" : ' + produitID + ', "number" : 1 }';
+      var prod = '{"id" : ' + produitID + ', "number" : 1, "size" : 0}';
       panier.push(JSON.parse(prod));
       return null;
+    },
+    push_to_panier: function(produitID, obj) {
+      for (var i = 0; i < panier.length; i++) {
+        if(panier[i].id === produitID) {
+          panier[i].size = obj;
+        }
+      }
+    },
+    get_panier: function (produitID) {
+      for (var i = 0; i < panier.length; i++) {
+        if(panier[i].id === produitID) {
+          return panier[i];
+        }
+      }
     },
     remove_from_panier: function(produitID) {
       for (var i = 0; i < panier.length; i++) {
@@ -43,36 +57,103 @@ angular.module('starter.services', [])
  };
 })
 
+.factory('Categories', function() {
+  var categories = [{
+    id: 0,
+    name: 'Packs',
+    prods: [5]
+  }, {
+    id: 1,
+    name:'Alcools',
+    prods: [3, 4]
+  }, {
+    id: 2,
+    name: 'Fumette',
+    prods: [0, 1, 2]
+  }]
+
+  return {
+    all: function() {
+      return categories;
+    }
+  }
+})
+
 .factory('Chats', function() {
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
   var chats = [{
     id: 0,
+    display: true,
     name: 'Ben Sparrow',
+    category: 2,
     description: 'You on your way?',
+    pack: false,
     pic: 'img/ben.png'
   }, {
     id: 1,
-    name: 'Max Lynx',
+    display: true,
+    name: 'Max Lyn',
+    category: 2,
     description: 'Hey, it\'s me',
+    pack: false,
     pic: 'img/max.png'
   }, {
     id: 2,
+    display: true,
     name: 'Adam Bradleyson',
+    category: 2,
     description: 'I should buy a boat',
+    pack: false,
     pic: 'img/adam.jpg'
   }, {
     id: 3,
+    display: true,
     name: 'Perry Governor',
+    category: 1,
     description: 'Look at my mukluks!',
+    pack: false,
     pic: 'img/perry.png'
   }, {
     id: 4,
+    display: true,
     name: 'Mike Harrington',
+    category: 1,
     description: 'This is wicked good ice cream.',
+    pack: false,
     pic: 'img/mike.png'
-  }];
+  }, {
+    id: 5,
+    display: true,
+    name: 'TekPaf',
+    category: 0,
+    description: 'Tequila+Lemon+Salt',
+    pack: [{
+        id : 0,
+        name: 'S',
+        description: 'Small',
+        products: [{
+          id: 0,
+          quantity: 2
+        }, {
+          id: 1,
+          quantity: 1
+        }]
+      }, {
+        id : 1,
+        name: 'L',
+        description: 'Large',
+        products: [{
+          id: 0,
+          quantity: 2
+        }, {
+          id: 1,
+          quantity: 3
+        }]
+      }],
+    pic: 'img/tekpaf.jpg'
+  }]
 
   return {
     all: function() {

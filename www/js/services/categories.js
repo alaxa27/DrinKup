0942a1eb65b26165aee29652ea5924ecc333f251
Taqiy <alaxa27@gmail.com>
2016-09-30 23:15:13 +0200

    prods

--- www/js/services/categories.js
+++ www/js/services/categories.js
@@ -6,8 +6,7 @@ drinkup.factory('Categories', function() {
   }, {
     id: 1,
     name:'Alcools',
-    prods: [0, 3, 4]
-  }, {
+    prods: [0, 1, 2, 3, 4, 5, 6, 7, 8]
     id: 2,
     name: 'Fumette',
     prods: [1, 2]
--- www/js/services/products.js
+++ www/js/services/products.js
@@ -8,21 +8,43 @@ drinkup.factory('Products', function () {
     options: [{
       id: 0,
       quantity: 0,
-      description: "Volume: 70cL",
+      description: "Volume: 70cL, 40% Vol.",
       price: 15
     }, {
       id: 1,
       quantity: 0,
-      description: "Volume: 1L",
+      description: "Volume: 1L, 40% Vol.",
       price: 20
     }, {
       id: 2,
       quantity: 0,
-      description: "Volume: 2L",
+      description: "Volume: 2L, 40% Vol.",
       price: 25
     }]
   }, {
-    id: 1,
+    id: 2,
+    display: true,
+    name: "Eristoff",
+    category: 1,
+    pic: "img/eristoff_70cL.jpg",
+    options: [{
+      id: 0,
+      quandtity: 0,
+      description: "Volume: 70cL, 40% Vol.",
+      price: 17
+    }, {
+      id: 1,
+      quantity: 0,
+      description: "Volume: 1L, 40% Vol.",
+      price: 25
+    }, {
+      id: 2,
+      quantity: 0,
+      description: "Volume 2L, 40% Vol.",
+      price: 30
+    }]
+  }, {
+    id: 2,
     display: true,
     name: "Jack Daniel\'s",
     category: 1,
@@ -30,20 +52,133 @@ drinkup.factory('Products', function () {
     options: [{
       id: 0,
       quantity: 0,
-      description: "Volume: 70cL",
+      description: "Volume: 70cL, 40% Vol.",
       price: 17
     }, {
       id: 1,
       quantity: 0,
-      description: "Volume: 1L",
+      description: "Volume: 1L, 40% Vol.",
       price: 22
     }, {
       id: 2,
       quantity: 0,
-      description: "Volume: 2L",
+      description: "Volume: 2L, 40%, Vol.",
       price: 30
     }]
-  }];
+  }, {
+    id: 3,
+    display: true,
+    name: "Ballantine\'s",
+    category: 1,
+    pic: "ballantines_70cL.jpg",
+    options: [{
+      id: 0,
+      quandtity: 0,
+      description: "Volume: 70cL, 40% Vol.",
+      price: 17
+    }, {
+      id: 1,
+      quantity: 0,
+      description: "Volume: 1L, 40% vol.",
+      price: 25
+    }, {
+      id: 2,
+      quantity: 0,
+      description: "Volume: 2L, 40% Vol.",
+      price: 30
+    }]
+  }, {
+    id: 4,
+    display: true,
+    name: "Tequila San José",
+    category: 1,
+    pic: "tequila_san_jose_70cL.jpg",
+    options: [{
+      id: 0,
+      quandtity: 0,
+      description: "Volume: 70cL, 35% Vol.",
+      price: 17
+    }, {
+      id: 1,
+      quantity: 0,
+      description: "Volume: 1L, 35% vol.",
+      price: 25
+    }, {
+      id: 2,
+      quantity: 0,
+      description: "Volume: 2L, 35% Vol.",
+      price: 30
+  }]
+}, {
+  id: 5,
+  display: true,
+  name: "Rhum blanc Saint James",
+  category: 1,
+  pic: "rhum_blanc_saint_james_70cL.jpg",
+  options: [{
+    id: 0,
+    quandtity: 0,
+    description: "Volume: 70cL, 40% Vol.",
+    price: 17
+  }, {
+    id: 1,
+    quantity: 0,
+    description: "Volume: 1L, 40% Vol.",
+    price: 25
+  }, {
+    id: 2,
+    quantity: 0,
+    description: "Volume: 2L, 40% Vol.",
+    price: 30
+  }]
+}, {
+  id: 6,
+  display: true,
+  name: "Fruits and Wine by Moncigale Pamplemousse",
+  category: 1,
+  pic: "fruits_and_wine_by_moncigale_pamplemousse_70cL.jpg",
+  options: [{
+    id: 0,
+    quandtity: 0,
+    description: "Volume: 75 cL, 7% Vol.",
+    price: 5
+  }]
+}, {
+  id: 7,
+  display: true,
+  name: "Fruits and Wine by Moncigale Pêche",
+  category: 1,
+  pic: "fruits_and_wine_by_moncigale_peche_70cL.jpg",
+  options: [{
+    id: 0,
+    quandtity: 0,
+    description: "Volume: 75 cL, 7% Vol.",
+    price: 5
+  }]
+}, {
+  id: 8,
+  display: true,
+  name: "Bières",
+  category: 1,
+  pic: "",
+  options: [{
+    id: 0,
+    quandtity: 0,
+    description: "",
+    price: 20
+  }, {
+    id: 1,
+    quantity: 0,
+    description: "",
+    price: 20
+  }, {
+    id: 2,
+    quantity: 0,
+    description: "",
+    price: 20
+  }]
+
+
 
   return {
     all: function () {
